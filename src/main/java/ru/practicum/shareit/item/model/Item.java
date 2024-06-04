1f503d16a441ff84692b54f49131fe5fa8fca888
SergeyIU <Murmansk.axsel@yandex.ru>
2024-06-04 09:15:56 +0300

    ShareIt-1

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.request.ItemRequest;
-import ru.practicum.shareit.user.model.User;
 
 /**
  * TODO Sprint add-controllers.
@@ -11,11 +10,16 @@ import ru.practicum.shareit.user.model.User;
 @Data
 @AllArgsConstructor
 public class Item {
-    private String id;
-    private String name;
-    private String description;
-    private String available;
-    private User owner;
-    private ItemRequest request;
 
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Boolean available;
+
+    private Long owner;
+
+    private ItemRequest request;
 }
